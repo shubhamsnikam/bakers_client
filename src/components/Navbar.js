89f698075268de7611b804787f2407b9a3f7ae14
Shubham Nikam <114049680+shubhamsnikam@users.noreply.github.com>
2025-05-13 16:28:44 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Navbar = () => (
   <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
-<<<<<<< HEAD
+
     <Link className="navbar-brand" to="/">  इंद्रायणी बेकर्स </Link>
-=======
-    <Link className="navbar-brand" to="/"> इंद्रायणी बेकर्स </Link>
->>>>>>> 6aaed51 (updated)
+
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
